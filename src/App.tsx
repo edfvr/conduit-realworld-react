cd0ba6e58fccd78fdd22421ebf6aac56ba42906a
Ed <edsagoefvr@gmail.com>
2024-07-12 20:49:58 +0000

    Create components and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import Login from './routes/Login';
+import Register from './routes/Register';
+import Settings from './routes/Settings';
+import Editor from './routes/Editor';
+import Article from './routes/Article';
+import Profile from './routes/Profile';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/editor" element={<Editor />} />
+            <Route path="/editor/:slug" element={<Editor />} />
+            <Route path="/article/:slug" element={<Article />} />
+            <Route path="/profile/:username" element={<Profile />} />
+            <Route path="/profile/:username/favorites" element={<Profile />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
-
-export default App;
